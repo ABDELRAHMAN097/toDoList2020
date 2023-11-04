ebfab11cabbaba0f86da1517131babaa9864cd37
boody <bodymagdy97@gmail.com>
2023-11-04 02:11:15 +0200

    d

--- main.js
+++ main.js
@@ -1,32 +1,19 @@
 var contaner = document.querySelector(".contaner");
-var btn = document.querySelector(".btn");
-
-function boomContent() {
-  contaner.classList.toggle("show");
-}
-btn.addEventListener("click", boomContent);
-
-var blusButton = document.querySelector(".blusButton");
+var plusButton = document.querySelector(".plusButton");
 var inputField = document.querySelector(".inputField");
 var toDoContaner = document.querySelector("#toDoContaner");
 var remover = document.querySelector(".remover");
 
-function intermsg() {
+plusButton.addEventListener("click", function(){
   var paragraph = document.createElement("p");
   paragraph.classList.add("paragaph-styling");
   paragraph.innerText = inputField.value;
   toDoContaner.appendChild(paragraph);
   inputField.value = "";
-  paragraph.addEventListener("click", function () {
+  paragraph.addEventListener("dblclick", function () {
     paragraph.style.textDecoration = "line-through";
   });
   paragraph.addEventListener("dblclick", function () {
     toDoContaner.removeChild(paragraph);
   });
-}
-
-
-blusButton.addEventListener("click", intermsg);
-document.querySelector(".inputField").oninput = function () {
-  console.log(this.value);
-};
+})  
